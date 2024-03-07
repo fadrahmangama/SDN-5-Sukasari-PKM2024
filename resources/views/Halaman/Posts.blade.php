1b679dc45b4098fc31360be0dad739faad553da9
fadrahmangama <fadrahmangama@student.telkomuniversity.ac.id>
2024-03-07 19:41:24 +0700

    Update Posts view

--- resources/views/Halaman/Posts.blade.php
+++ resources/views/Halaman/Posts.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.main')
 @section('section')
+    <section class="hero-section-2">
+        <div class="container d-flex align-items-center justify-content-center fs-1
+text-white flex-column">
+            <h1>Get to know about us!</h1>
+            <h2>SDN 5 Sukasari Tangerang</h2>
+        </div>
+    </section>
     <section class='container'>
         Ini halaman Blog
     </section>
